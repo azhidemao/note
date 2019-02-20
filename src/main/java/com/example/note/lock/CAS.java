e722270ace4833c4cb4ee02ec8969787767ec5d3
azhidemao <15718881030@163.com>
2019-02-20 15:24:54 +0800

    乐观锁(自旋)

--- src/main/java/com/example/note/lock/CAS.java
+++ src/main/java/com/example/note/lock/CAS.java
@@ -1,5 +1,7 @@
 package com.example.note.lock;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by JIAZ on 2019/2/20.
  * idea:先进行计算，如果期望值与内存值相同，则进行操作，否则不变
@@ -8,9 +10,10 @@ package com.example.note.lock;
 public class CAS {
     private volatile int memoryValue;
 
-    CAS(int memoryValue){
+    CAS(int memoryValue) {
         this.memoryValue = memoryValue;
     }
+
     public int lock(int inputValue) {
         while (true) {
             //先做操作得到结果
@@ -40,6 +43,18 @@ public class CAS {
     public static void main(String[] args) {
         CAS cas = new CAS(1);
         System.out.println(cas.lock(4));
+    }
+
+    private volatile int memoryValue2;
+
+    public synchronized int incrementAndGet() {
+        return ++memoryValue2;
+    }
+
+    private AtomicInteger memoryValue3 = new AtomicInteger();
+
+    public int incrementAndGet2() {
+        return memoryValue3.incrementAndGet();
     }
 
 }
