18f9cab026c33775ee48690010a06f7cad7212b3
azhidemao <15718881030@163.com>
2019-02-25 14:03:55 +0800

    冒泡排序

--- src/main/java/com/example/note/algorithm/sort/BubblingSort.java
+++ src/main/java/com/example/note/algorithm/sort/BubblingSort.java
@@ -6,10 +6,8 @@ package com.example.note.algorithm.sort;
 public class BubblingSort {
     void bubblingSort(int array[], int count){
         for (int i = 0; i < count; i++) {
-
             // 交换相邻的元素
             for (int j = 1; j < count - i; j++) {
-
                 if (array[j] < array[j-1]){
                     // 交换元素
                     int temp = array[j-1];
