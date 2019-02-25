bf85452960e5eeec07e497c7e01d2654d89620c2
azhidemao <15718881030@163.com>
2019-02-25 14:01:55 +0800

    选择排序

---
+++ src/main/java/com/example/note/algorithm/sort/BubblingSort.java
@@ -0,0 +1,23 @@
+package com.example.note.algorithm.sort;
+
+/**
+ * Created by JIAZ on 2019/2/25.
+ */
+public class BubblingSort {
+    void bubblingSort(int array[], int count){
+        for (int i = 0; i < count; i++) {
+
+            // 交换相邻的元素
+            for (int j = 1; j < count - i; j++) {
+
+                if (array[j] < array[j-1]){
+                    // 交换元素
+                    int temp = array[j-1];
+                    array[j-1] = array[j];
+                    array[j] = temp;
+                }
+
+            }
+        }
+    }
+}
