f48f576244d535c98fffb59727b0d332445bb805
azhidemao <15718881030@163.com>
2019-03-05 13:48:39 +0800

    volatile

---
+++ src/main/java/com/example/note/jvm/GCTimes.java
@@ -0,0 +1,30 @@
+package com.example.note.jvm;
+
+/**
+ * Created by JIAZ on 2019/3/5.
+ */
+public class GCTimes {
+
+    private static final int One_MB = 1024 * 1024;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("create a " + (i + 1) + " MB");
+            byte[] b1 = new byte[One_MB];
+        }
+
+        for (int i = 0; i < 4; i++) {
+            byte[] b2 = new byte[5 * One_MB];
+        }
+
+        for (int i = 0; i < 8; i++) {
+            System.out.println("create a " + (i + 1) + " MB");
+            byte[] b1 = new byte[One_MB];
+        }
+
+        for (int i = 0; i < 1; i++) {
+            byte[] b2 = new byte[5 * One_MB];
+        }
+    }
+
+}
