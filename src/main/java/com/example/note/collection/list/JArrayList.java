a5b62834d340e3b90876469f5a06898cc7b1d360
azhidemao <15718881030@163.com>
2019-03-06 16:53:24 +0800

    ArrayList

---
+++ src/main/java/com/example/note/collection/list/JArrayList.java
@@ -0,0 +1,14 @@
+package com.example.note.collection.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by JIAZ on 2019/3/6.
+ */
+public class JArrayList {
+    public static void main(String[] args) {
+        List list =new ArrayList();
+        Object[] obj = new Object[2];
+    }
+}
